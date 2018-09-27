9bf63e4cdf0a5130a2cfceb3838203dab4d4e447
yuqian <mercury0916@163.com>
2018-09-27 15:50:34 +0800

    lcd

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+module.exports = function main(x) {
+  var arr = x.split('').map(function(x){return x- '0'});
+
+   var lcd = {['._.', '|.|', '|_|'], ['...', '..|', '..|'], ['._.', '._|', '|_.'], ['._.', '._|', '._|'], ['...', '|_|', '..|'], ['._.', '|_.', '._|'], ['._.', '|_.', '|_|'], ['._.', '..|', '..|'], ['._.', '|_|', '|_|'], ['._.', '|_|', '..|']};
+
+
+for(var j = 0; j < arr.length; j++){
+     arr[j] = lcd[arr[j]];
+}
+
+console.log("arr.toString() + '\n'");
+    
+    return "arr.toString() + '\n'";
+}
